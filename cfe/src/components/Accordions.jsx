79b45d37d9d131effb8385e16706b288c02a1503
OhKaiitsKai <aymeealvarezv@gmail.com>
2023-12-04 13:31:45 -0600

    Falta mejorar: Accordions y Simulador de Consumos.

--- cfe/src/components/Accordions.jsx
+++ cfe/src/components/Accordions.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 
 const Accordions = () => {
   const [accordions, setAccordions] = useState([
-    { id: 1, isOpen: false },
-    { id: 2, isOpen: false },
+    { id: 1, isOpen: false, title: "Hogar", description: "Servicios para el hogar proporcionados por la CFE." },
+    { id: 2, isOpen: false, title: "Negocio", description: "Servicios para negocios ofrecidos por la CFE." },
+    { id: 3, isOpen: false, title: "Industria", description: "Servicios industriales proporcionados por la CFE." },
+    { id: 4, isOpen: false, title: "Agrícola y Servicios", description: "Servicios agrícolas y otros proporcionados por la CFE." },
     // Agrega más objetos aquí si necesitas más acordeones
   ]);
 
@@ -17,18 +19,19 @@ const Accordions = () => {
 
   return (
     <div className="accordions-container">
+      <h1 className="accordions-Servicios">Servicios</h1>
       {accordions.map((accordion) => (
         <div className="accordion" key={accordion.id}>
           <div
             className={`accordion-header ${accordion.isOpen ? "open" : ""}`}
             onClick={() => toggleAccordion(accordion.id)}
           >
-            <h2>Accordion Title {accordion.id}</h2>
+            <h2 className="accordion-title">{accordion.title}</h2>
             <span className={`icon ${accordion.isOpen ? "open" : ""}`}>&#x25BC;</span>
           </div>
           {accordion.isOpen && (
             <div className="accordion-content">
-              <p>Accordion Content Goes Here {accordion.id}</p>
+              <p>{accordion.description}</p>
             </div>
           )}
         </div>
