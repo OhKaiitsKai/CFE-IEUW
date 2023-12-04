e2fae45a5019b642505f379edfb3dfc00a3cd0c5
OhKaiitsKai <aymeealvarezv@gmail.com>
2023-12-04 04:19:36 -0600

    Agragar elementos en Accordions.jsx

--- cfe/src/components/Accordions.jsx
+++ cfe/src/components/Accordions.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
 
 const Accordions = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [accordions, setAccordions] = useState([
+    { id: 1, isOpen: false },
+    { id: 2, isOpen: false },
+    // Agrega más objetos aquí si necesitas más acordeones
+  ]);
 
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
+  const toggleAccordion = (accordionId) => {
+    setAccordions((prevAccordions) =>
+      prevAccordions.map((accordion) =>
+        accordion.id === accordionId ? { ...accordion, isOpen: !accordion.isOpen } : accordion
+      )
+    );
   };
 
   return (
-    <div className="accordion">
-      <div className={`accordion-header ${isOpen ? "open" : ""}`} onClick={toggleAccordion}>
-        <h2>Accordion Title</h2>
-        <span className={`icon ${isOpen ? "open" : ""}`}>&#x25BC;</span>
-      </div>
-      {isOpen && (
-        <div className="accordion-content">
-          <p>Accordion Content Goes Here</p>
+    <div className="accordions-container">
+      {accordions.map((accordion) => (
+        <div className="accordion" key={accordion.id}>
+          <div
+            className={`accordion-header ${accordion.isOpen ? "open" : ""}`}
+            onClick={() => toggleAccordion(accordion.id)}
+          >
+            <h2>Accordion Title {accordion.id}</h2>
+            <span className={`icon ${accordion.isOpen ? "open" : ""}`}>&#x25BC;</span>
+          </div>
+          {accordion.isOpen && (
+            <div className="accordion-content">
+              <p>Accordion Content Goes Here {accordion.id}</p>
+            </div>
+          )}
         </div>
-      )}
+      ))}
     </div>
   );
 };
